20622582021d8a8d351629897a61e64b1d870462
AbhiNeo <abhijit.satamkar@gmail.com>
2017-11-30 02:27:33 +0100

    Working Website

---
+++ webshopping/src/main/java/com/abhi/webshopping/controller/pagecontroller.java
@@ -0,0 +1,17 @@
+package com.abhi.webshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pagecontroller {
+	
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", "<h2>Hello from Abhi<h2>");
+		return mv;
+	}
+}
